c33e0c8fbca7993b869fb562353d168f013e5af0
Michael Spieler <michael.spieler@epfl.ch>
2016-10-08 20:46:22 +0200

    test simple type definition

--- type_compiler/test_type_compiler.py
+++ type_compiler/test_type_compiler.py
@@ -15,3 +15,20 @@ class TestParser(unittest.TestCase):
         expr, comment = split_line_in_expression_and_comment(line)
         self.assertEqual([], expr)
         self.assertEqual('', comment)
+
+    def test_parse_minimal_type_definition(self):
+        lines = [
+            'simple:',
+            '    int32 x'
+        ]
+        t = parse(iter(enumerate(lines)))
+        expect = TypeDefinition(
+            typename='simple',
+            docstring=[],
+            entries=[
+                TypeDefinitionEntry(
+                    type='int32',
+                    name='x',
+                    docstring='')
+            ])
+        self.assertEqual(next(t), expect)
